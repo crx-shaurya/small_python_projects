6fe289d4b0f86ae85fc408c58a478515c97f15d3
crx-shaurya <dhruvgupta9411@gmail.com>
2025-04-05 12:18:28 +0530

    Update main.py

--- 03_dice_roller_simulator/main.py
+++ 03_dice_roller_simulator/main.py
@@ -1,1 +1,18 @@
+import random
 
+def roll():
+    number=random.randint(1,6)
+    print("press enter to roll the dice or any other key to exit")
+    a=input()
+    if a == "":
+        print(f"you rolled a, {number}")
+    
+    elif a != "":
+        print("thanks for playing")
+        exit()
+
+while True:
+    try:
+        roll()
+    except:
+        break
